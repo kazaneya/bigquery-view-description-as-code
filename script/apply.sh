f673f186f7be907f41d28d8590ea93883d35f4d9
yuzutas0 <yuzutas0@users.noreply.github.com>
2021-08-13 22:08:09 +0900

    create apply shell script

---
+++ script/apply.sh
@@ -0,0 +1,9 @@
+for schema in `ls ../tables/*.json`; do
+    table_name=`basename　${schema} | sed 's/\.[^\.]*$//'`
+    query=`cat ../queries/${table_name}.sql`
+    echo '*****'
+    echo 'table_name: ${table_name}'
+    echo 'schema: ${schema}'
+    echo 'query: ${query}'
+    bq query '${query}' && bq update --schema=${schema} ${table_name}
+done
