bcb3daed11f66d881be4d649fa82f0ae7c1ec5ff
yuzutas0 <yuzutas0@users.noreply.github.com>
2021-08-14 10:21:34 +0900

    add sample dataset

--- script/apply.sh
+++ script/apply.sh
@@ -1,9 +1,12 @@
-for schema in $(ls ../tables/*.json); do
-    table_name=$(basename　${schema} | sed 's/\.[^\.]*$//')
-    query=$(cat ../queries/${table_name}.sql)
-    echo '*****'
-    echo 'table_name: ${table_name}'
-    echo 'schema: ${schema}'
-    echo 'query: ${query}'
-    bq query '${query}' && bq update --schema=${schema} ${table_name}
+for dataset_name in $(ls　-d ../tables/*); do
+    for schema in $(ls ../tables/${dataset_name}/*.json); do
+        table_name=$(basename　${schema} | sed 's/\.[^\.]*$//')
+        query=$(cat ../queries/${dataset_name}/${table_name}.sql)
+        echo '*****'
+        echo 'dataset_name: ${dataset_name}'
+        echo 'table_name: ${table_name}'
+        echo 'schema: ${schema}'
+        echo 'query: ${query}'
+        bq query '${query}' && bq update --schema=${schema} ${dataset_name}.${table_name}
+    done
 done
