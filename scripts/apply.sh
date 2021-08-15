1978707d750525e18fe1e92b94faf7a4acb90847
yuzutas0 <yuzutas0@users.noreply.github.com>
2021-08-15 17:04:25 +0900

    debug

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 echo '*** start ***'
 for dataset_name in $(ls -d -A ./tables/*/ | sed 's/\.\/tables\///g' | sed 's/\///g'); do
-    echo 'ls -la ./tables/${dataset_name}'
-    ls -la ./tables/${dataset_name}
-    echo 'ls -la ./tables/'
-    ls -la ./tables/
-    echo 'ls -la ./'
-    ls -la ./
-    echo 'ls -la ../'
-    ls -la ../
     for schema in $(ls ./tables/${dataset_name}/*.json); do
-        table_name=$(basename　${schema} | sed 's/\.[^\.]*$//')
+        which basename
+        table_name=$(basename　${schema} .json)
         query=$(cat ./queries/${dataset_name}/${table_name}.sql)
         echo '*****'
         echo 'dataset_name: ${dataset_name}'
